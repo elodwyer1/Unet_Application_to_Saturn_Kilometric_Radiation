9f5e6a4588f4ae2ff72501fe68c33a28686b4e8b
Beth O'Dwyer <odwyerbeth621@gmail.com>
2024-08-06 15:57:00 +0100

    Update train_model.py

---
+++ Scripts/train_model/__init__.py
@@ -0,0 +1,3 @@
+from .Train_Model import (PlotLossAccuracy, DataGen, Label_Generator,
+                          down_block, up_block, bottleneck, model,
+                          MLflowModelCheckpoint, plot_model_schematic)
